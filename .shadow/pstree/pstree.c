d78af28e4275ab96040cc194653b21feba9b1af1
tracer-nju <tracer@nju.edu.cn>
2023-06-12 14:02:51 -0500

    Linux moore.wot.eecs.northwestern.edu 4.18.0-477.10.1.el8_8.x86_64 #1 SMP Wed Apr 5 13:35:01 EDT 2023 x86_64 x86_64 x86_64 GNU/Linux  14:02:51 up 20 days,  4:59,  2 users,  load average: 2.58, 2.73, 2.77

---
+++ .shadow/pstree/pstree.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <assert.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <getopt.h>
+
+int main(int argc, char *argv[]) {
+  int opt;
+  static const struct option opts[3] = {
+    {"show-pids", no_argument, NULL, 'p'},
+    {"numeric-sort", no_argument, NULL, 'n'},
+    {"version", no_argument, NULL, 'v'}
+  };
+  while((opt = getopt_long(argc, argv, "p::n::v::", opts, NULL)) != -1) {
+    switch(opt) {
+    case 'p': {
+      break;
+    }
+    case 'n': {
+      break;
+    }
+    case 'v': {
+      break;
+    }
+    case '?':
+    default: {
+      fprintf(stderr, "please check the arguments\n");
+      exit(-1);
+    }
+    }
+  }
+  assert(!argv[argc]);
+  return 0;
+}
