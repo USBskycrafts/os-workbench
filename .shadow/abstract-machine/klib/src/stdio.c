677fca9095360157bc78cddd900f7d2f1a8e6cfc
tracer-nju <tracer@nju.edu.cn>
2023-06-19 21:44:14 +0000

    Linux ubuntu 5.15.0-1040-azure #47-Ubuntu SMP Thu Jun 1 19:38:24 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:44:14 up 19 min,  1 user,  load average: 0.04, 0.24, 0.24

--- .shadow/abstract-machine/klib/src/stdio.c
+++ .shadow/abstract-machine/klib/src/stdio.c
@@ -3,26 +3,43 @@
 #include <klib-macros.h>
 #include <stdarg.h>
 
-
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  panic("Not implemented");
+int printf(const char *fmt, ...)
+{
+  char buf[4096];
+  va_list ap;
+  va_start(ap, fmt);
+  int ret = vsnprintf(buf, 4095, fmt, ap);
+  va_end(ap);
+  return ret;
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+int vsprintf(char *out, const char *fmt, va_list ap)
+{
+  return vsnprintf(out, INT32_MAX, fmt, ap);
 }
 
-int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+int sprintf(char *out, const char *fmt, ...)
+{
+  va_list ap;
+  va_start(ap, fmt);
+  int ret = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return ret;
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+int snprintf(char *out, size_t n, const char *fmt, ...)
+{
+  va_list ap;
+  va_start(ap, fmt);
+  int ret = vsnprintf(out, n, fmt, ap);
+  va_end(ap);
+  return ret;
 }
 
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap)
+{
   panic("Not implemented");
 }
 
