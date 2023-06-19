72c3bc7c1b4630126bed7a4522b0472782cab512
tracer-nju <tracer@nju.edu.cn>
2023-06-19 23:20:32 +0000

    Linux ubuntu 5.15.0-1040-azure #47-Ubuntu SMP Thu Jun 1 19:38:24 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:20:32 up  1:55,  1 user,  load average: 0.01, 0.06, 0.02

--- .shadow/abstract-machine/klib/src/stdlib.c
+++ .shadow/abstract-machine/klib/src/stdlib.c
@@ -5,31 +5,71 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
 
-int rand(void) {
+int rand(void)
+{
   // RAND_MAX assumed to be 32767
   next = next * 1103515245 + 12345;
-  return (unsigned int)(next/65536) % 32768;
+  return (unsigned int)(next / 65536) % 32768;
 }
 
-void srand(unsigned int seed) {
+void srand(unsigned int seed)
+{
   next = seed;
 }
 
-int abs(int x) {
+int abs(int x)
+{
   return (x < 0 ? -x : x);
 }
 
-int atoi(const char* nptr) {
+int atoi(const char *nptr)
+{
   int x = 0;
-  while (*nptr == ' ') { nptr ++; }
-  while (*nptr >= '0' && *nptr <= '9') {
+  while (*nptr == ' ')
+  {
+    nptr++;
+  }
+  while (*nptr >= '0' && *nptr <= '9')
+  {
     x = x * 10 + *nptr - '0';
-    nptr ++;
+    nptr++;
   }
   return x;
 }
 
-void *malloc(size_t size) {
+void itoa(char *ptr, long long val)
+{
+  if (val >= 0)
+  {
+    utoa(ptr, val);
+  }
+  else
+  {
+    ptr[0] = '-';
+    unsigned long long tmp = ~(unsigned long long)val + 1;
+    utoa(ptr + 1, tmp);
+  }
+}
+
+void utoa(char *ptr, unsigned long long val)
+{
+  size_t cnt = 0;
+  while (val)
+  {
+    ptr[cnt++] = val % 10;
+    val /= 10;
+  }
+  for (int i = 0; i < cnt / 2; i++)
+  {
+    char tmp = ptr[i];
+    assert((int)cnt - 1 - i >= 0);
+    ptr[cnt - 1 - i] = tmp;
+    ptr[i] = ptr[cnt - 1 - i];
+  }
+}
+
+void *malloc(size_t size)
+{
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
@@ -39,7 +79,8 @@ void *malloc(size_t size) {
   return NULL;
 }
 
-void free(void *ptr) {
+void free(void *ptr)
+{
 }
 
 #endif
