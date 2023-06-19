0e1b18ed0c47e054b908316e0296e9b3b7a8d2a3
tracer-nju <tracer@nju.edu.cn>
2023-06-19 21:47:50 +0000

    Linux ubuntu 5.15.0-1040-azure #47-Ubuntu SMP Thu Jun 1 19:38:24 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:47:50 up 23 min,  1 user,  load average: 0.08, 0.14, 0.19

--- .shadow/abstract-machine/klib/src/string.c
+++ .shadow/abstract-machine/klib/src/string.c
@@ -4,43 +4,60 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-size_t strlen(const char *s) {
+size_t strlen(const char *s)
+{
+  size_t cnt = 0;
+  const char *ptr = s;
+  while (*ptr)
+  {
+    ptr++;
+    cnt++;
+  }
+  return cnt;
+}
+
+char *strcpy(char *dst, const char *src)
+{
   panic("Not implemented");
 }
 
-char *strcpy(char *dst, const char *src) {
+char *strncpy(char *dst, const char *src, size_t n)
+{
   panic("Not implemented");
 }
 
-char *strncpy(char *dst, const char *src, size_t n) {
+char *strcat(char *dst, const char *src)
+{
   panic("Not implemented");
 }
 
-char *strcat(char *dst, const char *src) {
+int strcmp(const char *s1, const char *s2)
+{
   panic("Not implemented");
 }
 
-int strcmp(const char *s1, const char *s2) {
+int strncmp(const char *s1, const char *s2, size_t n)
+{
   panic("Not implemented");
 }
 
-int strncmp(const char *s1, const char *s2, size_t n) {
+void *memset(void *s, int c, size_t n)
+{
   panic("Not implemented");
 }
 
-void *memset(void *s, int c, size_t n) {
+void *memmove(void *dst, const void *src, size_t n)
+{
   panic("Not implemented");
 }
 
-void *memmove(void *dst, const void *src, size_t n) {
+void *memcpy(void *out, const void *in, size_t n)
+{
   panic("Not implemented");
 }
 
-void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
-}
-
-int memcmp(const void *s1, const void *s2, size_t n) {
+int memcmp(const void *s1, const void *s2, size_t n)
+{
   panic("Not implemented");
 }
 
