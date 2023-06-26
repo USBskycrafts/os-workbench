a2d9fef41a1a0704c63292250c1ada70cfc302e9
tracer-nju <tracer@nju.edu.cn>
2023-06-26 22:08:26 +0000

    Linux ubuntu 5.15.0-1040-azure #47-Ubuntu SMP Thu Jun 1 19:38:24 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:08:26 up 7 days, 43 min,  1 user,  load average: 0.10, 0.05, 0.00

--- .shadow/abstract-machine/klib/src/string.c
+++ .shadow/abstract-machine/klib/src/string.c
@@ -14,15 +14,27 @@ size_t strlen(const char *s) {
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  return strncpy(dst, src, INT32_MAX);
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  size_t i;
+  for (i = 0; i < n && src[i] != '\0'; i++)
+      dst[i] = src[i];
+  for ( ; i < n; i++)
+      dst[i] = '\0';
+
+  return dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  size_t dest_len = strlen(dst);
+  size_t i;
+  for (i = 0 ; src[i] != '\0' ; i++) {
+    dst[dest_len + i] = src[i];
+  }
+  dst[dest_len + i] = '\0';
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
@@ -30,7 +42,15 @@ int strcmp(const char *s1, const char *s2) {
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  size_t i;
+  for(i = 0; i < n && *s1 && *s2; i++) {
+    if(s1[i] - s2[i] == 0) {
+      continue;
+    } else {
+      return s1[i] - s2[i];
+    }
+  }
+  return 0;
 }
 
 void *memset(void *s, int c, size_t n) {
