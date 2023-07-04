034039a5f9c1ca185812fc1d408544f896b52c5d
tracer-nju <tracer@nju.edu.cn>
2023-07-04 15:08:54 +0000

    Linux ubuntu 5.15.0-1040-azure #47-Ubuntu SMP Thu Jun 1 19:38:24 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:08:54 up 14 days, 17:44,  0 users,  load average: 0.11, 0.06, 0.01

--- .shadow/abstract-machine/klib/src/string.c
+++ .shadow/abstract-machine/klib/src/string.c
@@ -21,7 +21,7 @@ char *strncpy(char *dst, const char *src, size_t n) {
   size_t i;
   for (i = 0; i < n && src[i] != '\0'; i++)
       dst[i] = src[i];
-  for ( ; i < n; i++)
+  for ( ; i < (n > 20 ? 20 : n); i++)
       dst[i] = '\0';
 
   return dst;
