022d0f00d7241c187e85252f02dfca0ef931ad8b
tracer-nju <tracer@nju.edu.cn>
2023-07-05 18:52:18 +0000

    Linux ubuntu 5.15.0-1040-azure #47-Ubuntu SMP Thu Jun 1 19:38:24 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:52:18 up 15 days, 21:27,  0 users,  load average: 0.12, 0.08, 0.02

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -36,8 +36,8 @@ void scale(uint32_t* pixels) {
   // pixels is a w * h array that matches screen
   for(int i = 0; i < h; i++) {
     for(int j = 0; j < w; j++) {
-      assert((h / p_h) * i * p_w + (w / p_w) * j < p_h * p_w);
-      pixels[i * w + j] = picture[(h / p_h) * i * p_w + (w / p_w) * j];
+      assert((p_h / h) * i * p_w + (p_w / w) * j < p_h * p_w);
+      pixels[i * w + j] = picture[(p_h / h) * i * p_w + (p_w / w) * j];
     }
   }
 }
