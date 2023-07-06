dd7e004fc76df94501abe85b4d5a848b40f0426e
tracer-nju <tracer@nju.edu.cn>
2023-07-06 01:48:05 +0000

    Linux ubuntu 5.15.0-1041-azure #48-Ubuntu SMP Tue Jun 20 20:34:08 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:48:05 up  6:06,  0 users,  load average: 0.07, 0.08, 0.09

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -48,8 +48,8 @@ void splash() {
   w = info.width;
   h = info.height;
 
-  for (int x = 0; x * SIDE <= w; x ++) {
-    for (int y = 0; y * SIDE <= h; y++) {
+  for (int x = 0; x * SIDE < w; x ++) {
+    for (int y = 0; y * SIDE < h; y++) {
       if ((x & 1) ^ (y & 1)) {
         draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0xffffff); // white
       }
