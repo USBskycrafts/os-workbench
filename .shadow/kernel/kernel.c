0b8e0cf6226fccddfb411536299c8cb9f3c5f4fc
tracer-nju <tracer@nju.edu.cn>
2023-07-06 17:54:47 +0000

    Linux ubuntu 5.15.0-1041-azure #48-Ubuntu SMP Tue Jun 20 20:34:08 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:54:47 up 22:13,  0 users,  load average: 0.03, 0.12, 0.06

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -4,8 +4,8 @@
 #include <klib-macros.h>
 
 #define SIDE 16
-#define picW 1024
-#define picH 720 // Picture size
+#define picW 640
+#define picH 400 // Picture size
 
 static int w, h;  // Screen size
 
