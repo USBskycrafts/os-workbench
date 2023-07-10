dc623f0dd93c154cbe0429cbc1bab5a9d1964129
tracer-nju <tracer@nju.edu.cn>
2023-07-10 23:23:00 +0000

    Linux ubuntu 5.15.0-1041-azure #48-Ubuntu SMP Tue Jun 20 20:34:08 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:23:00 up 5 days,  3:41,  0 users,  load average: 0.27, 0.15, 0.04

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -1,15 +1,61 @@
 #include <common.h>
 
+
+#define MAGIC 0xff
+
+typedef struct __node_t {
+    int              size;
+    struct __node_t *next;
+} node_t;
+
+static node_t *head;
+
+
+
+static node_t* iterate(node_t *head, bool (*fun)(node_t*)) {
+  node_t *ptr = head;
+  while(ptr) {
+    if(fun(ptr)) {
+      return ptr;
+    }
+    ptr = ptr->next;
+  }
+  return NULL;
+}
+
+//**************************************
+// closure 
+static size_t msize;
+static bool find_greater(node_t* cur) {
+  if(cur->size >= msize) {
+    return true;
+  }
+  return false;
+}
+//**************************************
+
 static void *kalloc(size_t size) {
+  node_t *cur;
+  msize = size;
+  if((cur = iterate(head, find_greater)) != NULL) {
+    void* ret = (void*) cur + sizeof(node_t);
+    head = cur->next;
+    cur->next = (node_t*)0xff;
+    return ret;
+  }
   return NULL;
 }
 
 static void kfree(void *ptr) {
+
 }
 
 static void pmm_init() {
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
+  head = (node_t*)heap.start;
+  head->next = NULL;
+  head->size = pmsize;
 }
 
 MODULE_DEF(pmm) = {
