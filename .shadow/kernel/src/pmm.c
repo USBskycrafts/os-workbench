ed4a4b71c0e9c2c97bb3c7ffd07d1515ca90fc03
tracer-nju <tracer@nju.edu.cn>
2023-07-11 15:29:30 +0000

    Linux ubuntu 5.15.0-1041-azure #48-Ubuntu SMP Tue Jun 20 20:34:08 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:29:30 up 5 days, 19:48,  0 users,  load average: 0.00, 0.00, 0.00

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -1,78 +1,42 @@
 #include <common.h>
 
-#define MAGIC 0xff // warning: MAGIC must not alias to physical address
+#define MAGIC 0xff // warning: cannot overlap with physical address
+
+static size_t alias(size_t size) {
+  if(size == 0) {
+    return 0;
+  }
+  size_t msize = 2;
+  while(size > msize) {
+    msize <<= 1;
+  }
+  return msize;
+}
 
 typedef struct __node_t {
-    int              size;
-    struct __node_t *next;
+    int                size;
+    union {
+      int             magic;
+      struct __node_t *next;
+    };
 } node_t;
 
 static node_t *head;
 
-
-
-static node_t *iterate(node_t **head, bool (*fun)(node_t*)) {
-  while(*head) {
-    if(fun(*head)) {
-      return *head;
-    }
-    *head = (*head)->next;
-  }
-  return NULL;
-}
-
-//**************************************
-// closure 
-static size_t msize;
-static bool find_greater(node_t *cur) {
-  if(cur->size >= msize) {
-    return true;
-  }
-  return false;
-}
-//**************************************
-
 static void *kalloc(size_t size) {
-  node_t **cur = &head;
-  msize = size;
-  if(iterate(cur, find_greater) != NULL) {
-    void* ret = (void*) (*cur) + sizeof(node_t);
-    node_t *nxt = (*cur)->next;
-    (*cur) = nxt;
-    return ret;
-  }
+  size = alias(size);
   return NULL;
 }
-
-//**************************************
-// closure
-static node_t* fnode;
-static bool find_address(node_t *cur) {
-  if(cur->next > fnode) {
-    return true;
-  }
-  return false;
-}
-//**************************************
 
 static void kfree(void *ptr) {
-  node_t *nptr = ptr - sizeof(node_t);
-  node_t **cur = &head;
-  assert(nptr->next != (node_t*)MAGIC);
-  fnode = nptr;
-  iterate(cur, find_address);
-  node_t *nxt = (*cur)->next;
-  (*cur) = nptr;
-  nptr->next = nxt;
-  //then merging ipt, cur, cur->next if possible
 }
 
 static void pmm_init() {
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
-  head = (node_t*)heap.start;
-  head->next = NULL;
-  head->size = pmsize;
+  head = (node_t*) heap.start;
+  head->magic = MAGIC;
+  head->size = pmsize - sizeof(node_t);
 }
 
 MODULE_DEF(pmm) = {
