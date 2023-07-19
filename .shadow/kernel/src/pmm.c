4172b78d0006757758f0c4af1565cfa5d3641fd6
tracer-nju <tracer@nju.edu.cn>
2023-07-19 21:11:15 +0000

    Linux ubuntu 5.15.0-1041-azure #48-Ubuntu SMP Tue Jun 20 20:34:08 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:11:15 up 14 days,  1:30,  0 users,  load average: 0.70, 0.26, 0.14

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -50,7 +50,7 @@ static void pmm_init() {
     while((uintptr_t)ptr + (1 << (i + 1)) < (uintptr_t)heap.end) {
       ptr->isfree = 1;
       ptr->next = (node_t*)((char*)ptr + (1 << (i + 1)) + sizeof(node_t));
-      printf("a node at %p, size is %d\n", ptr, 1 << (i + 1));
+      printf("a node at %p, size is %x\n", ptr, 1 << (i + 1));
       ptr = ptr->next;
     }
   }
