a601a44d1d296dd6b2294463d1f47c7fd1b48502
Ubuntu <azureuser@ubuntu.mzkelmb1ldburmnszzncg2du1a.bx.internal.cloudapp.net>
2023-07-19 21:47:25 +0000

    pmm init implemented and tested, linked list implemented

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -1,5 +1,32 @@
 #include <common.h>
 
+typedef struct _node_t {
+  bool isfree;
+  union {
+    size_t size;
+    struct _node_t *next;
+  };
+} node_t;
+
+typedef struct {
+  int lock;
+  node_t *head;
+} slab_t;
+
+slab_t slab[24];
+
+
+void list_push_front(node_t **head, node_t *new) {
+  new->next = (*head);
+  (*head) = new;
+}
+
+node_t *list_pop_front(node_t **head) {
+    node_t *ret = (*head);
+    (*head) = (*head)->next;
+    return ret;
+}
+
 static void *kalloc(size_t size) {
   return NULL;
 }
@@ -10,6 +37,33 @@ static void kfree(void *ptr) {
 static void pmm_init() {
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
+  slab[23].head = heap.start;
+  slab[23].lock = 0;
+  node_t *ptr = slab[23].head;
+  // allocate 16MiB nodes
+  while((uintptr_t)ptr + (1 << 24) < (uintptr_t)heap.end) {
+    ptr->isfree = 1;
+    ptr->next = (node_t*)((char*)ptr + (1 << 24) + sizeof(node_t));
+    printf("a 16MiB node at %p\n", ptr);
+    ptr = ptr->next;
+  }
+  // make full use of the rest space
+  for(int i = 22; i >= 0; i--) {
+    slab[i].lock = 0;
+    if((uintptr_t)ptr + (1 << (i + 1)) < (uintptr_t)heap.end) {
+      slab[i].head = ptr;
+      printf("now init %x node\n", 1 << (i + 1));
+      while((uintptr_t)ptr + (1 << (i + 1)) < (uintptr_t)heap.end) {
+        ptr->isfree = 1;
+        ptr->next = (node_t*)((char*)ptr + (1 << (i + 1)) + sizeof(node_t));
+        printf("\ta node at %p\n", ptr);
+        ptr = ptr->next;
+      }
+    } else {
+      printf("cannot init %x node\n", 1 << (i + 1));
+      slab[i].head = NULL;
+    }
+  }
 }
 
 MODULE_DEF(pmm) = {
