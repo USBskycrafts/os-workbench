006ecd1ba6da8d9e3ffc565a5da10f5edc5cab2d
Ubuntu <azureuser@ubuntu.mzkelmb1ldburmnszzncg2du1a.bx.internal.cloudapp.net>
2023-07-19 23:36:28 +0000

    add 'SIZE2INDEX' and tested, fix the size of node and tested, implement list_remove, implement node_split and node_merge

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -1,4 +1,15 @@
 #include <common.h>
+#define INDEX2SIZE(index) (1 << (index + 1))
+#define SIZE2INDEX(size) ({    \
+  assert((size) != 0);         \
+  size_t msize = (size);       \
+  int cnt = 0;                 \
+  while(msize >> 1) {          \
+    cnt++;                     \
+    msize >>= 1;               \
+  }                            \
+  cnt - 1;                     \
+})                             \
 
 typedef struct _node_t {
   bool isfree;
@@ -13,7 +24,7 @@ typedef struct {
   node_t *head;
 } slab_t;
 
-slab_t slab[24];
+slab_t slab[24]; // size at index i is SIZE2INDEX(i) - sizeof(node_t)
 
 
 void list_push_front(node_t **head, node_t *new) {
@@ -22,9 +33,67 @@ void list_push_front(node_t **head, node_t *new) {
 }
 
 node_t *list_pop_front(node_t **head) {
-    node_t *ret = (*head);
-    (*head) = (*head)->next;
+  node_t *ret = (*head);
+  (*head) = (*head)->next;
+  return ret;
+}
+
+node_t *list_remove(node_t **head, node_t *target) {
+  node_t **cur = head;
+  while(*cur != NULL && *cur != target) {
+    cur = &((*cur)->next);
+  }
+  assert(*cur != NULL);
+  node_t *next = (*cur)->next;
+  (*cur) = next;
+  target->next = NULL;
+  return target;
+}
+
+node_t *node_split(node_t *prev, size_t target) {
+  // should mark prev as used in caller and offer prev a size
+  assert(prev->isfree == 0 && prev->size > target);
+  printf("the node that will split is %x, and the target size is %x", prev->size, target);
+  size_t size = (sizeof(node_t) + prev->size) / 2;
+  while(size > target) {
+    // insert second node in the buddy to free list 
+    node_t *next = (uintptr_t)prev + size + sizeof(node_t);
+    next->isfree = 1;
+    list_push_front(&(slab[SIZE2INDEX(size)]), next);
+    size = (sizeof(node_t) + prev->size) / 2;
+  }
+  prev->size = size;
+  return prev;
+}
+
+// if able to merge, return the new node's address, else return itself
+node_t *node_merge(node_t *prev) {
+  // must first mark prev as free in the caller
+  assert(prev->isfree == 1);
+
+  size_t size = prev->size + sizeof(node_t);
+  size_t index = SIZE2INDEX(size);
+  node_t *buddy = (node_t*)((uintptr_t)(prev) ^ (1 << (index + 1)));
+  printf("merge %x node, prev is %p, buddy is %p", size, prev, buddy);
+  if(buddy->isfree) {
+    // remove buddy from the slab
+    list_remove(&slab[index].head, buddy);
+
+    // build the merged node
+    node_t *ret = (node_t*)((uintptr_t)prev & (~(1 << (index + 1))));
+    // ret's address should be either prev or it's buddy
+    assert(ret == prev || ret == buddy);
+    ret->isfree = 1;
+    ret->size = size * 2 - sizeof(node_t);
+
+    // insert ret to the slab
+    list_push_front(&(slab[index].head), ret);
+
+    printf("node %p is able to merge, return node %p, size %x", prev, ret, ret->size);
     return ret;
+  } else {
+    return prev;
+  }
 }
 
 static void *kalloc(size_t size) {
@@ -41,26 +110,26 @@ static void pmm_init() {
   slab[23].lock = 0;
   node_t *ptr = slab[23].head;
   // allocate 16MiB nodes
-  while((uintptr_t)ptr + (1 << 24) < (uintptr_t)heap.end) {
+  while((uintptr_t)ptr + INDEX2SIZE(23) < (uintptr_t)heap.end) {
     ptr->isfree = 1;
-    ptr->next = (node_t*)((char*)ptr + (1 << 24) + sizeof(node_t));
+    ptr->next = (node_t*)((uintptr_t)ptr + INDEX2SIZE(23));
     printf("a 16MiB node at %p\n", ptr);
     ptr = ptr->next;
   }
   // make full use of the rest space
-  for(int i = 22; i >= 0; i--) {
+  for(int i = 22; i >= SIZE2INDEX(sizeof(node_t)); i--) {
     slab[i].lock = 0;
-    if((uintptr_t)ptr + (1 << (i + 1)) < (uintptr_t)heap.end) {
+    if((uintptr_t)ptr + INDEX2SIZE(i) < (uintptr_t)heap.end) {
       slab[i].head = ptr;
-      printf("now init %x node\n", 1 << (i + 1));
-      while((uintptr_t)ptr + (1 << (i + 1)) < (uintptr_t)heap.end) {
+      printf("now init %x node\n", INDEX2SIZE(i));
+      while((uintptr_t)ptr + INDEX2SIZE(i) < (uintptr_t)heap.end) {
         ptr->isfree = 1;
-        ptr->next = (node_t*)((char*)ptr + (1 << (i + 1)) + sizeof(node_t));
+        ptr->next = (node_t*)((char*)ptr + INDEX2SIZE(i));
         printf("\ta node at %p\n", ptr);
         ptr = ptr->next;
       }
     } else {
-      printf("cannot init %x node\n", 1 << (i + 1));
+      printf("cannot init %x node\n", INDEX2SIZE(i));
       slab[i].head = NULL;
     }
   }
