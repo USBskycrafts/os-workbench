056ec7687a97ff39e28df182ab3c6701a9572d01
tracer-nju <tracer@nju.edu.cn>
2023-07-20 04:49:16 +0000

    Linux ubuntu 5.15.0-1041-azure #48-Ubuntu SMP Tue Jun 20 20:34:08 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  04:49:16 up 14 days,  9:08,  0 users,  load average: 0.08, 0.06, 0.02

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -4,11 +4,21 @@ static void os_init() {
   pmm->init();
 }
 
+void* address[1000];
+
 static void os_run() {
   for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
     putch(*s == '*' ? '0' + cpu_current() : *s);
   }
-  while (1) ;
+  memset(address, 0, sizeof(address));
+  while(1) {
+    size_t index = rand() % 1000;
+    if(address[index] == NULL) {
+      address[index] = pmm->alloc(rand() % (1 << 24));
+    } else {
+      pmm->free(address[index]);
+    }
+  }
 }
 
 MODULE_DEF(os) = {
